2314a94b7672f40af60801d4ff6c0c970cc36245
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-02-22 23:56:02 -0800

    back tracking to login test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,5 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-
 function App() {
-  return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
-  );
+  return <h1>Hello from App</h1>;
 }
 
 export default App;
