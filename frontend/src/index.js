a79a995ecb4d01c7bf5bfa1c45b70ad4a4a66ba8
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-02-23 23:25:36 -0800

    Phase 3-complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import configureStore from "./store";
@@ -20,11 +20,11 @@ if (process.env.NODE_ENV !== "production") {
 
 function Root() {
   return (
-    <ReduxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ReduxProvider>
+    </Provider>
   );
 }
 
