5803aaa30d9435521e86fb2bb907d2f3c0326973
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-03-07 01:24:36 -0800

    group details complete

--- frontend/src/components/EventsForGroup/EventsForGroup.js
+++ frontend/src/components/EventsForGroup/EventsForGroup.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../GroupDetails/GroupDetails.css";
 const EventGroups = () => {
   const { groupId } = useParams();
   const dispatch = useDispatch();
@@ -15,22 +16,20 @@ const EventGroups = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h2>Events {events.length}</h2>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-            <img src={event.preview} alt={event.name} />
-            <div>
-              <h3>{event.title}</h3>
-              <p>{event.Group.city}</p>
-              <p>{event.Group.state}</p>
-              <p>{event.description}</p>
-              <p></p>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className="event-groups-container">
+      <h2>Events ({events.length})</h2>
+
+      {events.map((event) => (
+        <div>
+          <img src={event.preview} alt={event.name} />
+
+          <h3>{event.title}</h3>
+          <p>{event.Group.city}</p>
+          <p>{event.Group.state}</p>
+          <p>{event.description}</p>
+          <p></p>
+        </div>
+      ))}
     </div>
   );
 };
--- frontend/src/components/GroupDetails/GroupDetails.js
+++ frontend/src/components/GroupDetails/GroupDetails.js
@@ -8,6 +8,7 @@ import { useRef } from "react";
 import OpenModalButton from "../OpenModalButton";
 import { useHistory } from "react-router-dom";
 import EventGroups from "../EventsForGroup/EventsForGroup";
+import "./GroupDetails.css";
 
 const GroupDetail = () => {
   const user = useSelector((state) => state.session.user);
@@ -19,6 +20,7 @@ const GroupDetail = () => {
   const groups = useSelector((state) => {
     return state?.group.details;
   });
+  console.log(groups);
 
   useEffect(() => {
     dispatch(getGroupDetails(groupId));
@@ -55,69 +57,120 @@ const GroupDetail = () => {
   };
   return (
     <div>
-      {" "}
-      <p>
-        &lt; <NavLink to="/api/groups">Groups </NavLink>
-      </p>
+      <div className="breadcrumb-group-link">
+        &lt;
+        <NavLink className="get-all-groups" to="/api/groups">
+          {" "}
+          Groups{" "}
+        </NavLink>
+      </div>
+
       {groups && (
         <div>
-          <h1>{groups.name}</h1>
-          <img src={groups.preview} alt={groups.name} />
-          <p>{groups.city}</p>
-          <p>{groups.state}</p>
-          <div>
-            <p>{groups.numMembers} </p>
-            &middot;
-            {groups.private ? <p>Private</p> : <p>Public</p>}
-          </div>
-          <label>
-            Organized by: {groups.Organizer.firstName}{" "}
-            {groups.Organizer.lastName}
-          </label>
-          <div>
-            {user && user.id !== groups.organizerId && (
-              <button
-                onClick={() => alert("Feature coming soon")}
-                style={{ backgroundColor: "red" }}
-              >
-                Join the Group
-              </button>
-            )}
-          </div>
+          <div className="top-container">
+            <img
+              src={groups.GroupImages[0].url}
+              alt={groups.name}
+              style={{ width: "400px", height: "350px" }}
+            />
+            <div className="top-right-container">
+              <div className="top-four-items">
+                <div className="group-detail-name">
+                  <h1 style={{ marginBottom: "0px" }}>{groups.name}</h1>
+                </div>
+                <div className="three-middle-items">
+                  <div className="location-group">
+                    <p>{groups.city}</p>
+                    <p>{groups.state}</p>
+                  </div>
 
-          <h2>What we're about</h2>
-          <p>{groups.about}</p>
-        </div>
-      )}
-      {user && groups && user.id === groups.organizerId ? (
-        <div>
-          <NavLink to={`/api/groups/${groupId}/events`}>
-            <button>Create Event</button>
-          </NavLink>
-          <NavLink to={`/api/groups/${groupId}/update`}>
-            <button>Update</button>
-          </NavLink>
-          <button onClick={openMenu}>Delete</button>
-
-          {showMenu && (
-            //   <OpenModalButton>
-            <div className="delete-modal">
-              <h3> Confirm Delete</h3>
-              <p> Are you sure you want to remove this group?</p>
-              <button className="delete-button" onClick={handleDelete}>
-                Yes (Delete Group)
-              </button>
-              <button className="keep-button" onClick={closeMenu}>
-                No (Keep Group)
-              </button>
+                  <div className="number-of-events-privacy">
+                    <p>Number of Events: {groups.numMembers} </p>
+                    <p>&middot;</p>
+                    {groups.private ? <p>Private</p> : <p>Public</p>}
+                  </div>
+                  <div className="organizer-group-detail">
+                    <p style={{ color: "#666666" }}>
+                      Organized by: {groups.Organizer.firstName}{" "}
+                      {groups.Organizer.lastName}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div></div>
+              <div className="group-manage-buttons-container">
+                {user && user.id !== groups.organizerId && (
+                  <button
+                    className="group-join-button"
+                    onClick={() => alert("Feature coming soon")}
+                    // style={{
+                    //   backgroundColor: "#D0312D",
+                    //   width: "160px",
+                    //   height: "40px",
+                    //   color: "white",
+                    //   fontWeight: "bold",
+                    //   boxShadow: "3px 3px 5px #000000",
+                    //   border: "1px solid black",
+                    // }}
+                  >
+                    Join the Group
+                  </button>
+                )}
+                {user && groups && user.id === groups.organizerId ? (
+                  <div>
+                    <div className="manage-group-container">
+                      <NavLink to={`/api/groups/${groupId}/events`}>
+                        <button className="manage-buttons">Create Event</button>
+                      </NavLink>
+                      <NavLink to={`/api/groups/${groupId}/update`}>
+                        <button className="manage-buttons">Update</button>
+                      </NavLink>
+                      <button className="manage-buttons" onClick={openMenu}>
+                        Delete
+                      </button>
+                    </div>
+                    {showMenu && (
+                      //   <OpenModalButton>
+                      <div className="delete-modal">
+                        <h3> Confirm Delete</h3>
+                        <p> Are you sure you want to remove this group?</p>
+                        <button
+                          className="delete-button"
+                          onClick={handleDelete}
+                        >
+                          Yes (Delete Group)
+                        </button>
+                        <button className="keep-button" onClick={closeMenu}>
+                          No (Keep Group)
+                        </button>
+                      </div>
+                      //   </OpenModalButton>
+                    )}
+                  </div>
+                ) : (
+                  <br />
+                )}
+              </div>
             </div>
-            //   </OpenModalButton>
-          )}
+          </div>
+
+          <div className="group-bottom-container">
+            <h2 className="organizer-title">Organizer </h2>
+            <div className="organizer-values">
+              <p>{groups.Organizer.firstName}</p>
+              <p>{groups.Organizer.lastName}</p>
+            </div>
+            <div className="about-groups-title">
+              <h2>What we're about</h2>
+            </div>
+            <div className="about-groups">
+              <p>{groups.about}</p>
+            </div>
+          </div>
         </div>
-      ) : (
-        <br />
       )}
-      <div className="events-group-list">
+
+      <div className="events-bottom-container">
         <EventGroups />
       </div>
     </div>
