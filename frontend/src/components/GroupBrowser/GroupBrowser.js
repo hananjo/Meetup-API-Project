e64114b75a37542ed05acc6173d83d293c27f404
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-03-06 17:38:29 -0800

    css

--- frontend/src/components/GroupBrowser/GroupBrowser.js
+++ frontend/src/components/GroupBrowser/GroupBrowser.js
@@ -20,27 +20,122 @@ const GroupBrowser = () => {
 
   return (
     <div>
-      <NavLink to="/api/events">
-        {" "}
-        <h2>Events</h2>
-      </NavLink>
+      <div style={{ margin: "0 auto", width: "50%" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "0px",
+            alignItems: "40%",
+          }}
+        >
+          <NavLink
+            to="/api/events"
+            style={{
+              marginRight: "20px",
+              textDecoration: "none",
+              color: "#666666",
+            }}
+            onMouseEnter={(e) => (e.target.style.textDecoration = "underline")}
+            onMouseLeave={(e) => (e.target.style.textDecoration = "none")}
+          >
+            {" "}
+            <h2>Events</h2>
+          </NavLink>
 
-      <h2>Groups</h2>
-      {groups?.map((group) => {
-        return (
-          <div key={group.id}>
-            <NavLink key={group.id} to={`/api/groups/${group.id}`}>
-              <img src={group.preview} />
-              <h2>{group.name}</h2>
-              <p>{group.city}</p>
-              <p>{group.state}</p>
-              <p>{group.about}</p>
-              <p>{group.numMembers}</p>
-              {group.private ? <p>Private</p> : <p>Public</p>}
+          <h2 style={{ color: "teal", textDecoration: "underline" }}>Groups</h2>
+        </div>
+        <h4
+          style={{
+            color: "lightgray",
+            marginTop: "-5px",
+          }}
+        >
+          Groups in Meetup
+        </h4>
+      </div>
+      <div style={{ width: "50%", margin: "0 auto" }}>
+        {groups?.map((group) => {
+          return (
+            // <div key={group.id}>
+            <NavLink
+              key={group.id}
+              to={`/api/groups/${group.id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  borderTop: "1px solid black",
+                }}
+              >
+                <img
+                  src={group.preview}
+                  style={{
+                    width: "200px",
+                    // marginLeft: "400px",
+                    marginRight: "30px",
+                    marginBottom: "30px",
+                    marginTop: "30px",
+                  }}
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <h3
+                    style={{
+                      color: "black",
+                      fontSize: "25px",
+                      marginBottom: ".25%",
+                    }}
+                  >
+                    {group.name}
+                  </h3>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      gap: "10px",
+                      color: "#666666",
+                    }}
+                  >
+                    {" "}
+                    <p>{group.city}</p>
+                    <p>{group.state}</p>
+                  </div>
+                  <p style={{ color: "black", marginTop: ".5px" }}>
+                    {group.about}
+                  </p>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      gap: "10px",
+                      marginRight: "20px",
+                      color: "#666666",
+                      marginBottom: "15%",
+                    }}
+                  >
+                    <p>Number of Events: {group.numMembers} </p>
+                    <p>&middot;</p>
+                    {group.private ? <p>Private</p> : <p>Public</p>}
+                  </div>
+                </div>
+              </div>{" "}
+              {/* <h2 style={{ display: "flex", marginLeft: "400px" }}>
+                _______________________________________________________
+              </h2> */}
             </NavLink>
-          </div>
-        );
-      })}
+
+            // </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -9,11 +9,11 @@ const LandingPage = () => {
     <div className="container">
       <section className="section1">
         <div className="title-info">
-          <h1>
+          <h1 style={{ fontSize: "40px" }}>
             Welcome to Meetup! Here you'll find like-minded friends with common
             interests{" "}
           </h1>
-          <p>Join a group, meet a friend!</p>
+          <p style={{ fontSize: "30px" }}>Join a group, meet a friend!</p>
         </div>
         <div className="infographic">
           <img
@@ -25,48 +25,114 @@ const LandingPage = () => {
       </section>
       <section className="section2">
         <div className="subTitle-info">
-          <h2>How it works</h2>
-          <p>Meet your people, discover yourself, enjoy your life!</p>
+          <h2 style={{ fontSize: "25px", textAlign: "center" }}>
+            How it works
+          </h2>
+          <p style={{ fontSize: "20px", textAlign: "center" }}>
+            Meet your people, discover yourself, enjoy your life!
+          </p>
         </div>
       </section>
-      <section className="section3">
-        <div className="group-column">
-          <NavLink to="/api/groups">
+      {/* <section className="section3"> */}
+      {/* <div className="column"> */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "15px",
+          // justifyContent: "space-evenly",
+        }}
+      >
+        <NavLink
+          to="/api/groups"
+          style={{
+            color: "teal",
+            textAlign: "center",
+            textDecoration: "none",
+            cursor: "pointer",
+            fontSize: "20px",
+          }}
+          // onMouseOver={(e) => (e.target.style.textDecoration = "underline")}
+          onMouseEnter={(e) => (e.target.style.textDecoration = "underline")}
+          onMouseLeave={(e) => (e.target.style.textDecoration = "none")}
+        >
+          <img
+            src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677548492/Project%20Schema/groups_igjkqj.jpg"
+            alt="Meetup Pic"
+            width="180"
+            height="150"
+          />
+          <p>See all groups</p>
+        </NavLink>
+        {/* </div> */}
+        {/* <div className="column"> */}
+        <NavLink
+          to="/api/events"
+          style={{
+            color: "teal",
+            textAlign: "center",
+            textDecoration: "none",
+            cursor: "pointer",
+            fontSize: "20px",
+          }}
+          onMouseEnter={(e) => (e.target.style.textDecoration = "underline")}
+          onMouseLeave={(e) => (e.target.style.textDecoration = "none")}
+        >
+          <img
+            src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677549283/Project%20Schema/33-339853_calendar-invite-cartoon_xoat94.png"
+            alt="Find Event"
+            width="230"
+            height="150"
+          />
+          <p>Find an event</p>
+        </NavLink>
+        {/* </div> */}
+        {/* <div className="column"> */}
+        {user ? (
+          <NavLink
+            to="/api/groups/new"
+            // className={user ? "not-disabled" : "disabled"}
+            style={{
+              color: "teal",
+              textAlign: "center",
+              textDecoration: "none",
+              cursor: "pointer",
+              fontSize: "20px",
+            }}
+            onMouseEnter={(e) => (e.target.style.textDecoration = "underline")}
+            onMouseLeave={(e) => (e.target.style.textDecoration = "none")}
+          >
             <img
-              src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677548492/Project%20Schema/groups_igjkqj.jpg"
-              alt="Meetup Pic"
+              src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677549283/Project%20Schema/crowd-clipart-event-13_ocahsp.png"
+              alt="Create-Event"
+              width="230"
             />
-            <p>See all groups</p>
+            <p>Start a new group</p>
           </NavLink>
-        </div>
-        <div className="event-column">
-          <NavLink to="/api/events">
-            <img
-              src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677549283/Project%20Schema/33-339853_calendar-invite-cartoon_xoat94.png"
-              alt="Find Event"
-            />
-            <p>Find an event</p>
-          </NavLink>
-        </div>
-        <div className="create-group-column">
-          {user ? (
-            <NavLink
-              to="/api/groups/new"
-              // className={user ? "not-disabled" : "disabled"}
-            >
-              <img
-                src="https://res.cloudinary.com/dwphwqyrn/image/upload/v1677549283/Project%20Schema/crowd-clipart-event-13_ocahsp.png"
-                alt="Create-Event"
-              />
-              <p>Start a new group</p>
-            </NavLink>
-          ) : (
-            <br />
-          )}
-        </div>
-      </section>
+        ) : (
+          <br />
+        )}
+        {/* </div> */}
+      </div>
+      {/* </section> */}
       <section className="section4">
-        <button>Join Meetup</button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button
+            style={{
+              backgroundColor: "teal",
+              borderRadius: "0px",
+              padding: "10px 22px",
+              fontSize: "15px",
+              fontWeight: "bold",
+              color: "white",
+              fontFamily: "Helvetica, sans-serif",
+              boxShadow: "2px 2px 0px 0px #000",
+              borderColor: "5px #000",
+            }}
+          >
+            Join Meetup
+          </button>
+        </div>
       </section>
     </div>
   );
