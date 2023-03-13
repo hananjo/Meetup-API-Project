ac0df657e6a9fc20ab2066cd806457e282fb952b
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-03-12 19:22:08 -0700

    ordering events

--- frontend/src/components/EventBrowser/EventBrowser.js
+++ frontend/src/components/EventBrowser/EventBrowser.js
@@ -16,6 +16,16 @@ const EventBrowser = () => {
     dispatch(getAllEvents());
   }, [dispatch]);
 
+  const now = new Date();
+
+  const upComingEvents = events.filter(
+    (event) => new Date(event.startDate) >= now
+  );
+  const pastEvents = events.filter((event) => new Date(event.startDate) < now);
+  upComingEvents.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+
+  pastEvents.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
   return (
     <div>
       <div className="get-all-nav">
@@ -39,7 +49,61 @@ const EventBrowser = () => {
         </div>
       </div>
       <div className="get-all-top-container">
-        {events?.map((event) => {
+        {upComingEvents?.map((event) => {
+          return (
+            <div key={event.id}>
+              <NavLink
+                // key={event.id}
+                to={`/api/events/${event.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="get-all-listings">
+                  <div className="get-all-img">
+                    <img src={event.preview} style={{ width: "200px" }} />
+                  </div>
+                  <div className="listing-info">
+                    <div className="startDate">
+                      <p>
+                        {new Date(event.startDate).toString().split(" ")[3]}
+                        {" / "}
+                        {new Date(event.startDate).toString().split(" ")[1]}
+                        {" / "}
+                        {new Date(event.startDate).toString().split(" ")[2]}
+                        {"   "}
+                        &middot;
+                        {"    "}
+                        {new Date(event.startDate).toString().split(" ")[4]}
+                      </p>
+                    </div>
+                    <div className="listing-name">
+                      <h2>{event.name}</h2>
+                    </div>
+                    <div className="event-price">
+                      <p>Price: ${event.price}</p>
+                    </div>
+                    <div className="event-capacity">
+                      <p>Capacity: {event.capacity}</p>
+                    </div>
+                    <div className="event-type">
+                      <p>{event.type}</p>{" "}
+                    </div>
+                    <div className="event-location">
+                      {" "}
+                      {event && event.Group && <p>{event?.Group.state}</p>}
+                    </div>
+                  </div>
+                </div>
+                <div className="event-description">
+                  {" "}
+                  <p>{event.description}</p>
+                </div>
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
+      <div className="get-all-top-container">
+        {pastEvents?.map((event) => {
           return (
             <div key={event.id}>
               <NavLink
